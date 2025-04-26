fb366f3fc42d3be7ec4d3fb61ff51c32c350daa6
Tony Von Wolfe <tonyvonwolfe@gmail.com>
2025-04-26 09:16:53 -0600

    rename `year` column to `model_year`

--- spec/factories/vehicles.rb
+++ spec/factories/vehicles.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :vehicle do
     user
     vin { Faker::Vehicle.vin }
-    year { Faker::Date.between(from: '1925-01-01'.to_date, to: Date.current).year }
+    model_year { Faker::Date.between(from: '1925-01-01'.to_date, to: Date.current).year }
     manufacturer { Faker::Vehicle.manufacturer }
     model { Faker::Vehicle.model }
   end
