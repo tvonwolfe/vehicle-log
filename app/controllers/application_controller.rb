1f85046f5f39b78492024818c43dd0d35ec53108
Tony Von Wolfe <17915221+tvonwolfe@users.noreply.github.com>
2025-06-08 11:39:04 -0600

    add generic 404 page (#82)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   include Authentication
   allow_browser versions: { ie: false }
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+  def render_not_found
+    render Views::NotFound
+  end
 end
