1a46bbde612926a1b7f66a818af5e0dd73da381a
Tony Von Wolfe <17915221+tvonwolfe@users.noreply.github.com>
2025-07-11 21:36:51 -0600

    Add cancan gem (#93)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   allow_browser versions: { ie: false }
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+  rescue_from CanCan::AccessDenied, with: :render_not_found
 
   def render_not_found
     render Views::NotFound
